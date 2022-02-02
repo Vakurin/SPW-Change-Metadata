7b8ac235b5724fb1b3a7ff839b998c80dc4a2518
hokage229 <ernestzulpukarov@gmail.com>
2022-02-03 00:10:14 +0700

    refactor

---
+++ alive_mints_from_metadata.py
@@ -0,0 +1,11 @@
+from find import find_mint_ids_by_att
+from utility import read_json, write_file
+
+
+def find_alive_mint_ids(data):
+    alive_mint_ids = find_mint_ids_by_att(data, "isAlive", "True")
+    write_file(data=alive_mint_ids, name='data/alive_mint_ids_by_metadata')
+
+
+metadata = read_json("data/spw_metadata.json")
+find_alive_mint_ids(metadata)
--- find.py
+++ find.py
@@ -1,15 +1,7 @@
-import random
-
 from utility import read_json
 from utility import write_file
 
 DEAD_IMG_URL = "https://arweave.net/RHLYg5wZwpCX3ZwwZYAJTriJo1ZkLB2ruGHbfy6GfJc"
-
-
-# extra function to find from *_metadata.json
-def find_alive_mint_ids(data):
-    alive_mint_ids = find_mint_ids_by_att(data, "isAlive", "True")
-    write_file(data=alive_mint_ids, name='data/alive_mint_ids_by_metadata')
 
 
 def find_mint_ids_by_att(data, att_name, att_value):
@@ -40,7 +32,7 @@ def find_mint_ids_by_name(data, names_to_kill):
     return mint_ids
 
 
-def kill_mint_ids(data, mint_ids_to_kill: list):
+def kill_mint_ids(data, mint_ids_to_kill: list, all_ids: list):
     dead_ids = read_json("data/dead_mint_ids.json")
     for el in data:
         mint = el['mint']
@@ -53,76 +45,13 @@ def kill_mint_ids(data, mint_ids_to_kill: list):
                 if mint not in dead_ids:
                     dead_ids.append(mint)
     write_file(dead_ids, 'data/dead_mint_ids')
-    subtract_deads_from_alive()
+    subtract_deads_from_alive(all_ids)
 
 
-def subtract_deads_from_alive():
-    all_ids: list = read_json("data/ssj_mint_ids.json")  # change
+def subtract_deads_from_alive(all_ids: list):
     dead_ids: list = read_json("data/dead_mint_ids.json")
     alive_ids = []
     for el in all_ids:
         if el not in dead_ids:
             alive_ids.append(el)
     write_file(alive_ids, 'data/alive_mint_ids')
-
-
-def get_holders_with_alive_mints(mints_number: int):
-    holders_json: dict = read_json('data/ssj_holders.json')  # change
-    alive_ids: list = read_json('data/alive_mint_ids.json')
-
-    holders = dict()
-    for value, key in holders_json.items():
-        alive_mints = list(filter(lambda mint: mint in alive_ids, key['mints']))
-        if len(alive_mints) > mints_number:
-            holders[value] = alive_mints
-    write_file(holders, 'test')
-    return holders
-
-
-def get_mint_att_dict(data: list, att_name):
-    mint_att_dict = dict()
-    for el in data:
-        mint = el['mint']
-        for att in el['metadata']['attributes']:
-            if att['trait_type'] == att_name:
-                mint_att_dict[mint] = att
-    return mint_att_dict
-
-
-# get mints if holder has mints with different trait values
-def get_mints_from_holders(holders: dict, mints_number: int, mint_att: dict):
-    result_mints = []
-    for holder, mints in holders.items():
-        temp = mint_att.get(mints[0])
-        for mint in mints:
-            print(mint)
-            if mint_att.get(mint) != temp:
-                result_mints.extend(random.sample(mints, mints_number))
-                print("__")
-                break
-    print(result_mints)
-    return result_mints
-
-
-# # get alive mint ids from metadata
-# metadata = read_json("data/spw_metadata.json")
-# find_alive_mint_ids(metadata)
-
-# # kill from traits
-# metadata = read_json("data/spw_metadata.json")
-# ids = find_mint_ids_by_att(metadata, "Mustache", "Mechanical Mustache")
-# kill_mint_ids(metadata, ids)
-
-# # kill from names
-# metadata = read_json("data/spw_metadata.json")
-# names = read_json("data/names_to_kill.json")
-# ids = find_mint_ids_by_name(metadata, names)
-# kill_mint_ids(metadata, ids)
-
-# # kill mints with different traits in holder
-metadata = read_json("data/ssj_metadata.json")
-mints_per_holder = 5
-ids = get_mints_from_holders(get_holders_with_alive_mints(mints_per_holder),
-                             mints_per_holder,
-                             get_mint_att_dict(metadata, 'Background'))
-kill_mint_ids(metadata, ids)
