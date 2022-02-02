6debb5fd73427340df873dd6e6e833b89965fd1f
hokage229 <ernestzulpukarov@gmail.com>
2022-02-03 00:36:36 +0700

    refactor

--- src/utility.py
+++ src/utility.py
@@ -2,10 +2,10 @@ import json
 
 
 def read_json(filename):
-    f = open(filename)
+    f = open('../' + filename)
     return json.load(f)
 
 
 def write_file(data, name):
-    with open(f'./{name}.json', 'w', encoding='utf-8') as f:
+    with open(f'../{name}.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
